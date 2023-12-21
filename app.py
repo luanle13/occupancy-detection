7b9806245f55701e8b1082ae6b0e9c082d2e7c23
Luan Le <thanhluan130201@gmail.com>
2023-12-21 10:11:05 +0700

    feat: something

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ import pandas as pd
 import os
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from flask_cors import CORS
+import pickle
 
 
 app = Flask(__name__)
 CORS(app)
 # options = tf.saved_model.LoadOptions(experimental_io_device='/job:localhost')
-model = load_model('./model.h5')
+with open('svm_model.pkl', 'rb') as model_file:
+    loaded_svm_model = pickle.load(model_file)
 df_train = pd.read_csv('datatrain.csv')
 df_train = df_train.drop('Occupancy', axis=1).drop('date', axis=1)
 scaler = StandardScaler()
@@ -51,7 +53,7 @@ def postprocess(prediction):
 def predict():
     data = request.get_json(force=True)
     input_data = preprocess(data)
-    prediction = model.predict(input_data)
+    prediction = loaded_svm_model.predict(input_data)
     result = postprocess(prediction)
     return jsonify(result)
 
